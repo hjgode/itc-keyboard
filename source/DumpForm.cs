5bc75684efcffae4acfa776079f736ff65e0bebd
hjgode <hjgode@11ad59bc-01a9-c773-de9f-35ed3b7438ca>
2010-10-31 08:52:58 +0000

    The whole VS source tree

---
+++ source/DumpForm.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CUsbKeysCStest
+{
+    public partial class DumpForm : Form
+    {
+        public DumpForm()
+        {
+            InitializeComponent();
+            ITC_KEYBOARD.CUSBkeys _cusbKeys = new ITC_KEYBOARD.CUSBkeys();
+            textBox1.Text = _cusbKeys.dumpAllKeys();
+        }
+
+        private void mnuBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void mnuSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sd = new SaveFileDialog();
+            if (sd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (System.IO.StreamWriter sw = new System.IO.StreamWriter(sd.FileName))
+                    {
+                        // Add some text to the file.
+                        sw.WriteLine(textBox1.Text);
+                        sw.Flush();
+                    }
+                    MessageBox.Show("Dump saved to '" + sd.FileName + "'");
+                }
+                catch (System.IO.IOException iox)
+                {
+                    MessageBox.Show("Exception in write file: " + iox.Message);
+                }
+            }
+        }
+    }
+}
