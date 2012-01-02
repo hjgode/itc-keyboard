520541c2e0464534778ec1f1feeba69cdb2c81cb
hjgode <hjgode@11ad59bc-01a9-c773-de9f-35ed3b7438ca>
2012-01-02 15:21:44 +0000

    Added sample to map F1 to 4 x TAB plus ENTER

---
+++ trunk/F1toTABS/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace F1toTABS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            F1toTABSTABS mapKeys = new F1toTABSTABS();
+            mapKeys.mapF1toTABTABS();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ITC_KEYBOARD.CUSBkeys cusb = new ITC_KEYBOARD.CUSBkeys();
+            if (cusb.resetKeyDefaults() != 0)
+                MessageBox.Show("You must reboot befor the changes become active");
+        }
+
+        private void mnuExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
