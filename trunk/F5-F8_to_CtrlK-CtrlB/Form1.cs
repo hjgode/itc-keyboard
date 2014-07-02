e88b18667a429dc74b0aba2aa5fced4467b77a6c
hjgode <hjgode@11ad59bc-01a9-c773-de9f-35ed3b7438ca>
2014-07-02 13:54:30 +0000

    added tool to map F5-F6 to Ctrl-K, Ctrl-L, Ctrl-N and Ctrl-B on a CK71 alphanumeric

---
+++ trunk/F5-F8_to_CtrlK-CtrlB/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace F5_F8_to_CtrlK_CtrlB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
