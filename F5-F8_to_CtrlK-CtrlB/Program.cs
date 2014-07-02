f8e1d79c826628e2e5b8ead552d18302c34810ab
hjgode <hjgode@11ad59bc-01a9-c773-de9f-35ed3b7438ca>
2014-07-02 13:54:30 +0000

    added tool to map F5-F6 to Ctrl-K, Ctrl-L, Ctrl-N and Ctrl-B on a CK71 alphanumeric

---
+++ F5-F8_to_CtrlK-CtrlB/Program.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+using ITC_KEYBOARD;
+
+namespace F5_F8_to_CtrlK_CtrlB
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [MTAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                if (args[0].Contains("default"))
+                {
+                    ITC_KEYBOARD.CUSBkeys cusb = new ITC_KEYBOARD.CUSBkeys();
+                    if (cusb.resetKeyDefaults() != 0)
+                        MessageBox.Show("You must reboot befor the changes become active");
+                    else
+                        MessageBox.Show("Keyboard reset to default");
+                }
+            }
+            else
+            {
+                mapKeys();
+            } 
+            Application.Run(new Form1());
+
+        }
+        static void mapKeys()
+        {
+            remapF5_to_F8_to_CtrlKLNB mapKeys = new remapF5_to_F8_to_CtrlKLNB();
+            mapKeys.mapKeys();
+
+        }
+
+    }
+    class remapF5_to_F8_to_CtrlKLNB
+    {
+        ITC_KEYBOARD.CUSBkeys _cusbKeys;
+        CUsbKeyTypes.HWkeys[] keysToMap = new CUsbKeyTypes.HWkeys[] { CUsbKeyTypes.HWkeys.Left_Control };
+
+        public remapF5_to_F8_to_CtrlKLNB()
+        {
+            _cusbKeys = new CUSBkeys();
+        }
+
+        /// <summary>
+        /// map the function keys:
+        /// F4 to Ctrl+n
+        /// F5 to Ctrl+g
+        /// </summary>
+        public void mapKeys()
+        {
+            keysToMap = new CUsbKeyTypes.HWkeys[4];
+            keysToMap[0] = CUsbKeyTypes.HWkeys.F5_cozumel;
+            keysToMap[1] = CUsbKeyTypes.HWkeys.F1_cozumel;//orange plane F1 == F6
+            keysToMap[2] = CUsbKeyTypes.HWkeys.F2_cozumel;//orange plane F2 == F7
+            keysToMap[3] = CUsbKeyTypes.HWkeys.F3_cozumel;//orange plane F3 == F8
+
+            createMultiKeys();
+        }
+
+        /// <summary>
+        /// create multikey entries with Ctrl + Letters
+        /// Control is 08,02,01,14
+        /// </summary>
+        public void createMultiKeys()
+        {
+            /*
+            leftCtrl key is 
+                07,E0,00,00,08,01 'Left Control' [NoFlag,NoFlag,ModifierIndex,] 'F9' 'ModifierKey'->08,02,01,14 'Left Control' | 
+            */
+            //F1 key:   = 07 3A 00 00 00 05
+            // ########## as multi4 key:        07,3a,00,02,04,04
+
+            //prepare a new multikey entry for two keys
+            CUSBkeys.usbKeyStructShort[] uKey = new CUSBkeys.usbKeyStructShort[2];  // we need Ctrl+n
+            //map to Ctrl 
+            //find Ctrl modifier index
+            ITC_KEYBOARD.CModifiersKeys _cModis = new CModifiersKeys();
+            int iModiCtrlIndex = findCtrlModifier();
+
+            //create a key struct that points to the ctrl key modifier entry
+            uKey[0].bFlagHigh = CUsbKeyTypes.usbFlagsHigh.NoFlag;
+            uKey[0].bFlagMid = CUsbKeyTypes.usbFlagsMid.NoFlag;
+            uKey[0].bFlagLow = CUsbKeyTypes.usbFlagsLow.ModifierIndex;
+            uKey[0].bIntScan = (byte)iModiCtrlIndex;  //point to modifier index
+            //create an entry for k
+            uKey[1].bFlagHigh = CUsbKeyTypes.usbFlagsHigh.NoFlag;
+            uKey[1].bFlagMid = CUsbKeyTypes.usbFlagsMid.NoFlag;
+            uKey[1].bFlagLow = CUsbKeyTypes.usbFlagsLow.NormalKey;
+            uKey[1].bIntScan = (byte)ITC_KEYBOARD.PS2KEYS.K;  //point to K key
+
+            //try to find existing MultiKey
+            CMultiKeys cmulti = new CMultiKeys();
+            int iMax = cmulti.getMultiKeyCount();
+            int iFoundCtrlK = cmulti.findMultiKey(uKey);
+            if (iFoundCtrlK == -1)//if there was no existing entry, create a new one
+            {
+                iFoundCtrlK = cmulti.addMultiKey(uKey);
+            }
+
+            //create another multikey entry for Ctrl+L
+            //create a key struct that points to the ctrl key modifier entry
+            uKey[0].bFlagHigh = CUsbKeyTypes.usbFlagsHigh.NoFlag;
+            uKey[0].bFlagMid = CUsbKeyTypes.usbFlagsMid.NoFlag;
+            uKey[0].bFlagLow = CUsbKeyTypes.usbFlagsLow.ModifierIndex;
+            uKey[0].bIntScan = (byte)iModiCtrlIndex;  //point to modifier index
+            //map to g
+            uKey[1].bFlagHigh = CUsbKeyTypes.usbFlagsHigh.NoFlag;
+            uKey[1].bFlagMid = CUsbKeyTypes.usbFlagsMid.NoFlag;
+            uKey[1].bFlagLow = CUsbKeyTypes.usbFlagsLow.NormalKey;
+            uKey[1].bIntScan = (byte)ITC_KEYBOARD.PS2KEYS.L;  //point to G key
+            //is this already known?
+            int iFoundCtrlL = cmulti.findMultiKey(uKey);
+            if (iFoundCtrlL == -1)//if there was no existing entry, create a new one
+            {
+                iFoundCtrlL = cmulti.addMultiKey(uKey);
+            }
+
+            //create another multikey entry for Ctrl+N
+            //create a key struct that points to the ctrl key modifier entry
+            uKey[0].bFlagHigh = CUsbKeyTypes.usbFlagsHigh.NoFlag;
+            uKey[0].bFlagMid = CUsbKeyTypes.usbFlagsMid.NoFlag;
+            uKey[0].bFlagLow = CUsbKeyTypes.usbFlagsLow.ModifierIndex;
+            uKey[0].bIntScan = (byte)iModiCtrlIndex;  //point to modifier index
+            //map to g
+            uKey[1].bFlagHigh = CUsbKeyTypes.usbFlagsHigh.NoFlag;
+            uKey[1].bFlagMid = CUsbKeyTypes.usbFlagsMid.NoFlag;
+            uKey[1].bFlagLow = CUsbKeyTypes.usbFlagsLow.NormalKey;
+            uKey[1].bIntScan = (byte)ITC_KEYBOARD.PS2KEYS.N;  //point to G key
+            //is this already known?
+            int iFoundCtrlN = cmulti.findMultiKey(uKey);
+            if (iFoundCtrlN == -1)//if there was no existing entry, create a new one
+            {
+                iFoundCtrlN = cmulti.addMultiKey(uKey);
+            }
+
+            //create another multikey entry for Ctrl+B
+            //create a key struct that points to the ctrl key modifier entry
+            uKey[0].bFlagHigh = CUsbKeyTypes.usbFlagsHigh.NoFlag;
+            uKey[0].bFlagMid = CUsbKeyTypes.usbFlagsMid.NoFlag;
+            uKey[0].bFlagLow = CUsbKeyTypes.usbFlagsLow.ModifierIndex;
+            uKey[0].bIntScan = (byte)iModiCtrlIndex;  //point to modifier index
+            //map to g
+            uKey[1].bFlagHigh = CUsbKeyTypes.usbFlagsHigh.NoFlag;
+            uKey[1].bFlagMid = CUsbKeyTypes.usbFlagsMid.NoFlag;
+            uKey[1].bFlagLow = CUsbKeyTypes.usbFlagsLow.NormalKey;
+            uKey[1].bIntScan = (byte)ITC_KEYBOARD.PS2KEYS.B;  //point to G key
+            //is this already known?
+            int iFoundCtrlB = cmulti.findMultiKey(uKey);
+            if (iFoundCtrlB == -1)//if there was no existing entry, create a new one
+            {
+                iFoundCtrlB = cmulti.addMultiKey(uKey);
+            }
+
+            //now map the key(s) in question to the new/existing MultiKeyEntry in all planes
+            //CUSBkeys.usbKeyStruct remapKey = new CUSBkeys.usbKeyStruct();
+
+            //############### map the keys to the new multikeys #####################
+            //map F1 key in plane x to new multikey
+#if DEBUG
+            keysToMap[0] = CUsbKeyTypes.HWkeys.g;
+            keysToMap[1] = CUsbKeyTypes.HWkeys.h;
+            setKeyToMultiKey((int)keysToMap[0], (byte)(iFoundCtrlN), cPlanes.plane.green);
+            setKeyToMultiKey((int)keysToMap[1], (byte)(iFoundCtrlG), cPlanes.plane.green);
+#else
+            setKeyToMultiKey((int)keysToMap[0], (byte)(iFoundCtrlK), cPlanes.plane.normal);
+            setKeyToMultiKey((int)keysToMap[1], (byte)(iFoundCtrlL), cPlanes.plane.orange);
+            setKeyToMultiKey((int)keysToMap[2], (byte)(iFoundCtrlN), cPlanes.plane.orange);
+            setKeyToMultiKey((int)keysToMap[3], (byte)(iFoundCtrlB), cPlanes.plane.orange);
+#endif
+            //save and update
+            int iRes = _cusbKeys.writeKeyTables();
+            if (iRes != 0)
+                MessageBox.Show("Need reboot");
+        }
+
+        /// <summary>
+        /// map a key to a multikey index
+        /// </summary>
+        /// <param name="iKey">the key to map</param>
+        /// <param name="iMultiIndex">zero based index to Multi1, Multi2...</param>
+        /// <param name="cPlane">keyboard plane</param>
+        private void setKeyToMultiKey(int iKey, byte iMultiIndex, cPlanes.plane cPlane)
+        {
+            CUSBkeys.usbKeyStruct remapKey = new CUSBkeys.usbKeyStruct();
+
+            if (_cusbKeys.getKeyStruct((int)cPlane, iKey, ref remapKey) != -1)
+            {   //key exists
+                remapKey.bFlagHigh = CUsbKeyTypes.usbFlagsHigh.NoFlag;
+                remapKey.bFlagMid = CUsbKeyTypes.usbFlagsMid.NoRepeat | CUsbKeyTypes.usbFlagsMid.NoChord;
+                remapKey.bFlagLow = CUsbKeyTypes.usbFlagsLow.MultiKeyIndex;
+                remapKey.bIntScan = (byte)(iMultiIndex + 1); //the idx is zero based! but it is Multi1, Multi2..
+                _cusbKeys.setKey((int)cPlane, iKey, remapKey);
+            }
+            else
+            {
+                //add key
+                remapKey.bHID = 0x07;
+                remapKey.bScanKey = (CUsbKeyTypes.HWkeys)iKey;
+                remapKey.bFlagHigh = CUsbKeyTypes.usbFlagsHigh.NoFlag;
+                remapKey.bFlagMid = CUsbKeyTypes.usbFlagsMid.NoRepeat | CUsbKeyTypes.usbFlagsMid.NoChord;
+                remapKey.bFlagLow = CUsbKeyTypes.usbFlagsLow.MultiKeyIndex;
+                remapKey.bIntScan = (byte)iMultiIndex;
+                _cusbKeys.addKey(cPlane, remapKey);
+            }
+            System.Diagnostics.Debug.WriteLine("setKeyToMultiKey: " + _cusbKeys.dumpKey(remapKey));
+        }
+
+        /// <summary>
+        /// find the Modifiers Entry for the "Control Key"
+        /// creates a new Modifier Entry if no entry is found
+        /// </summary>
+        /// <returns>-1 if not found
+        /// pos value is index of found/created entry</returns>
+        private int findCtrlModifier()
+        {
+            //  08,02,01,14 'Left Control'
+            //assemble the Ctrl modifier entry: 01,02,08,14[StickyOnce,NoRepeat,ModifierIndex,] 'Left Control'
+            CUSBkeys.usbKeyStructShort uKey = CUSBkeys.controlModKey;// new CUSBkeys.usbKeyStructShort();
+            int iFound = -1;
+            CModifiersKeys cModis = new CModifiersKeys();
+            iFound = cModis.findModifierKey(uKey);
+
+            if (iFound == -1)
+                iFound = cModis.addModifierKey(uKey);
+
+            return iFound;
+        }
+
+        public void restoreDefaultMappings()
+        {
+            CUSBkeys cusb = new CUSBkeys();
+            cusb.resetKeyDefaults();
+        }
+    }
+}
